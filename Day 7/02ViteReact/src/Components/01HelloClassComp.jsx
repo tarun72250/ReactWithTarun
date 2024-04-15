f2a25307ce31a45f2b28b6c2fc4f7e63aa66afa6
tarun72250 <tarunrathore2904@gmail.com>
2024-04-15 18:33:32 +0530

    add 8 day work

---
+++ Day 7/02ViteReact/src/Components/01HelloClassComp.jsx
@@ -0,0 +1,29 @@
+/*import React from "react";
+
+class HelloClassComp extends React.Component {
+
+    render()
+    {
+        return(
+            <>
+                <h1>Hello From Class Component</h1>
+            </>
+        )
+    }
+}
+
+export default HelloClassComp;
+*/
+import React from "react";
+
+class HelloClassComp extends React.Component {
+  render() {
+    return (
+      <>
+        <h1>Hello from class comp</h1>
+      </>
+    );
+  }
+}
+
+export default HelloClassComp;
